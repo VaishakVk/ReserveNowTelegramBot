31258764c9f6449a478cdc5a1f09dd51cabfda5d
Vaishak V K <vaishakhvkv@gmail.com>
2018-04-11 00:36:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,62 @@
-# Copyright 2016 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import webapp2
 import urllib
-import urllib2
 import json
-#import requests
+import logging
+from google.appengine.ext import ndb
 
-
-class MainPage(webapp2.RequestHandler):
-    def post(self):
+class CurrentConversation(ndb.Model):
+	ChatId = ndb.IntegerProperty()
+	Message = ndb.StringProperty()
+	CreationDate = ndb.DateTimeProperty(auto_now_add=True)
+	
+class BotInstructions():
+	def __init__(self, fname, lname, chat_id, chat_message):
+		self.fname = fname
+		self.lname = lname
+		self.chat_id = chat_id
+		self.chat_message = chat_message
+		self.response = None
+		self.cleaned_message = chat_message
+		
 		# Requests and Responses
-		list_greetings = ['Hello', 'Hi', 'Hii', 'Howdy', 'Oi', 'Ey', 'Hey', 'Heyy', 'Oii']
+		self.list_greetings = ['Hello', 'Hi', 'Hii', 'Howdy', 'Oi', 'Ey', 'Hey', 'Heyy', 'Oii']
 		
-		list_request_date = ['When would you like me to book a table for you?', 'When are you planning to visit us again?', 'Can I know when you would be visiting us?']
-		list_request_time = ['And at what time?', 'Around what time can we expect your presence?', 'At what time would you visit us?']
-		list_request_ppl = ['How many persons would be accompanying you ?', 'How many guests should we expect?', 'Let me know how many persons are accompanying you.']
-		list_request_table_type = ['We offer pool view, roof top and In the house. Which one would you prefer?', 'We have some of the best seats waiting or you - pool view, roof top and In the house. Please select your preference.', 'How about making a selection - pool view, roof top and In the house']
-		list_request_confirmation = ['You have opted for {type} table and you would be visiting us on {date} at {time}. Can I confirm', 'Please confirm. The following is your preference - {type} table visiting us on {date} at {time}']
+		self.list_request_date = ['When would you like me to book a table for you?', 'When are you planning to visit us again?', 'Can I know when you would be visiting us?']
+		self.list_request_time = ['And at what time?', 'Around what time can we expect your presence?', 'At what time would you visit us?']
+		self.list_request_ppl = ['How many persons would be accompanying you ?', 'How many guests should we expect?', 'Let me know how many persons are accompanying you.']
+		self.list_request_table_type = ['We offer pool view, roof top and In the house. Which one would you prefer?', 'We have some of the best seats waiting or you - pool view, roof top and In the house. Please select your preference.', 'How about making a selection - pool view, roof top and In the house']
+		self.list_request_confirmation = ['You have opted for {type} table and you would be visiting us on {date} at {time}. Can I confirm', 'Please confirm. The following is your preference - {type} table visiting us on {date} at {time}']
 		
-		list_response_success = ['Congrats!! We have successfully reserved a table for you.', 'Great News!! You table booking is confirmed.', 'Well!! We have booked the required table for you.']
-		list_response_no_seats = ['Uh Oh, looks like we do not have the required availability.', 'Sorry but we are facing heavy demand', 'Sorry, the bookings are full at the moment']
-
+		self.list_response_success = ['Congrats!! We have successfully reserved a table for you.', 'Great News!! You table booking is confirmed.', 'Well!! We have booked the required table for you.']
+		self.list_response_no_seats = ['Uh Oh, looks like we do not have the required availability.', 'Sorry but we are facing heavy demand', 'Sorry, the bookings are full at the moment']
+		
+		#Junk characters
+		self.list_junk_char = [',', '.', '-', '!', '$', '=', '#', '%', '^', '*', '(', ')','"']
+	
+	def remove_junk(self):
+		for i in self.list_junk_char:
+			self.cleaned_message = self.cleaned_message.replace(i, '')
+			#logging.debug(i + ' ' + self.cleaned_message)
+		return self.cleaned_message.strip()
+			
+	def get_response(self):
+		# Insert data into database
+		Conv = CurrentConversation(ChatId = self.chat_id, Message = self.chat_message)
+		Conv.put()
+		
+		conv_query = CurrentConversation.query(CurrentConversation.ChatId == self.chat_id) ##.order(-CurrentConversation.CreationDate)
+		for i in conv_query:
+			latest_conversation = i.Message
+			break
+		
+		#query = ndb.gql('SELECT Message FROM CurrentConversation WHERE ChatId = :1 ORDER BY CreationDate DESC LIMIT 1', self.chat_id)
+		self.cleaned_message = self.remove_junk()
+		self.response = 'Hello ' + self.fname + ' ' + self.lname + ', Welcome to ReserveNow !!'
+		return latest_conversation
+		
+class MainPage(webapp2.RequestHandler):
+	
+	def post(self):
 
 		# Convert to JSON
 		data_json = json.loads(self.request.body)
@@ -46,7 +70,10 @@ class MainPage(webapp2.RequestHandler):
 		chat_message = data_json['message']['text']
 		
 		# Send message
-		response = 'Hello ' + f_name + ' ' + l_name + ', Welcome to ReserveNow !!'
+		bot_response = BotInstructions(fname = f_name, lname = l_name, chat_id = chat_id, chat_message = chat_message)
+		response = bot_response.get_response()
+		
+		#response = 'Hello ' + f_name + ' ' + l_name + ', Welcome to ReserveNow !!'
 		message_url = 'https://api.telegram.org/bot/sendMessage?text='+response+'&chat_id='+str(chat_id)
 		urllib.urlopen(message_url)
 		
