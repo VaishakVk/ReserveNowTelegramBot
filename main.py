ffbf48fadcebce1f2cd43458a8196e47f379aa41
Vaishak V K <vaishakhvkv@gmail.com>
2018-04-10 17:18:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,13 +21,16 @@ import json
 
 class MainPage(webapp2.RequestHandler):
     def post(self):
+		# Requests and Responses
+		list_greetings = ['Hello', 'Hi', 'Hii', 'Howdy', 'Oi', 'Ey', 'Hey', 'Heyy', 'Oii']
+		
 		list_request_date = ['When would you like me to book a table for you?', 'When are you planning to visit us again?', 'Can I know when you would be visiting us?']
 		list_request_time = ['And at what time?', 'Around what time can we expect your presence?', 'At what time would you visit us?']
 		list_request_ppl = ['How many persons would be accompanying you ?', 'How many guests should we expect?', 'Let me know how many persons are accompanying you.']
 		list_request_table_type = ['We offer pool view, roof top and In the house. Which one would you prefer?', 'We have some of the best seats waiting or you - pool view, roof top and In the house. Please select your preference.', 'How about making a selection - pool view, roof top and In the house']
 		list_request_confirmation = ['You have opted for {type} table and you would be visiting us on {date} at {time}. Can I confirm', 'Please confirm. The following is your preference - {type} table visiting us on {date} at {time}']
 		
-		list_response_success = ['Congrats!! We ave successfully reserved a table for you.', 'Great News!! You table booking is confirmed.', 'Well!! We have booked the required table for you.']
+		list_response_success = ['Congrats!! We have successfully reserved a table for you.', 'Great News!! You table booking is confirmed.', 'Well!! We have booked the required table for you.']
 		list_response_no_seats = ['Uh Oh, looks like we do not have the required availability.', 'Sorry but we are facing heavy demand', 'Sorry, the bookings are full at the moment']
 
 
